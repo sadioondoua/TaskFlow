c5c2190f9ef917a01f4a60a48f0543166606d169
sadioondoua <ondouasadio@gmail.com>
2026-07-04 09:25:56 +0100

    test: Modification du tests

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,10 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(
-    0,
-    str(Path(__file__).resolve().parent.parent / "src")
-)
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "src"))
 
 from models import Task
 from storage import (
